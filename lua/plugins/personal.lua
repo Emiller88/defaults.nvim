e975e784a933c0feaeb4577b0b65e617b6f40bbc
Edmund Miller <edmund.a.miller@protonmail.com>
2023-02-15 21:08:53 -0600

    feat: Add initial lazyvim config

---
+++ lua/plugins/personal.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "nvim-orgmode/orgmode",
+    ft = {"org"},
+    -- setup_ts_grammar(),
+    opts = {org_agenda_files = {"~/sync/org/*"},org_default_notes_file = "~/sync/org/todo.org"},
+  },
+  { 'Mxrcon/nextflow-vim'},
+  {'wuelnerdotexe/vim-astro'},
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  }
+}
